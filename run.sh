8541b453b471c394f09c355cbd9e39f107585e61
Megha Malali <malalimegha@gmail.com>
2016-04-05 18:50:19 -0400

    Delete run.sh

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-javac -cp "src;src/guava-19.0.jar;src/json-simple-1.1.1.jar" -d "src" "src/*.java"
-java -cp "src/guava-19.0.jar;src/json-simple-1.1.1.jar;src" MainClass "./tweet_input/tweets.txt" "./tweet_output/output.txt"
