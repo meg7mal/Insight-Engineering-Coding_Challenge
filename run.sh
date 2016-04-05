ab0494923e72e91bda18e1350758ac219de44364
Megha Malali <malalimegha@gmail.com>
2016-04-05 18:07:29 -0400

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
-javac -cp "lib/guava-19.0.jar;lib/json-simple-1.1.1.jar" src/*.java
-java -cp "lib/guava-19.0.jar;lib/json-simple-1.1.1.jar;src" MainClass "./tweet_input/tweets.txt" "./tweet_output/output.txt"
+#!/bin/bash
+javac -cp "src;src/guava-19.0.jar;src/json-simple-1.1.1.jar" -d "src" "src/*.java"
+java -cp "src/guava-19.0.jar;src/json-simple-1.1.1.jar;src" MainClass "./tweet_input/tweets.txt" "./tweet_output/output.txt"
